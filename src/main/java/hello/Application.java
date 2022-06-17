3180a68a95aed59b468f812d5b02f7dd2db3ae77
vincent121416 <vincent121416@yahoo.com.hk>
2022-06-17 16:58:48 +0800

    Update Application.java

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -8,10 +8,20 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
+import java.util.Stack;
 
 @SpringBootApplication
 @RestController
 public class Application {
+
+  private String href = null;
+  private Stack<String> cmdSeq;
+  private int healthNum = 0;
+  private int curScore = 0;
+  private boolean pointIncrease;
+  private PlayerState playerState;
+  private String direction;
+  private Map<String, PlayerState> arenaState;
 
   static class Self {
     public String href;
@@ -56,10 +66,48 @@ public class Application {
   @PostMapping("/**")
   public String index(@RequestBody ArenaUpdate arenaUpdate) {
     System.out.println(arenaUpdate);
-    String[] commands = new String[]{"F", "R", "L", "T"};
-    int i = new Random().nextInt(4);
-    return commands[i];
+    if (null == this.href) {
+      this.href = arenaUpdate._links.self.href;
+    }
+
+    this.playerState = arenaUpdate.arena.state.get(this.href);
+    this.direction = playerState.direction;
+    
+    if (playerState.wasHit) {
+      healthNum += 1;
+    } else {
+      healthNum = 0;
+    }
+
+    pointIncrease = playerState.score > curScore;
+    curScore = playerState.score;
+
+    if (cmdSeq.empty()) {
+      if (healthNum > 3) {
+        moveRandom();
+        fireTwice();
+      } else if (pointIncrease) {
+        return "T";
+      } else {
+        moveRandom();
+        fireTwice();
+      }
+    }
+    
+    return cmdSeq.pop();
   }
 
+  private void moveRandom() {
+    cmdSeq.push("F");
+    cmdSeq.push("F");
+    String[] LF = new String[]{ "R", "L"};
+    int i = new Random().nextInt(1);
+    cmdSeq.push(LF[i]);
+  }
+
+  private void fireTwice() {
+    cmdSeq.push("T");
+    cmdSeq.push("T");
+  }
 }
 
